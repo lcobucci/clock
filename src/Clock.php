fddf1b1c2397249c9041bd9fb8de713826f17ce8
Andreas Heigl <andreas@heigl.org>
2022-04-17 11:31:07 +0200

    Implement forward compatible Clock-Interface
    The stella-maris/clock package provides an interface based on the
    currently proposed status of PSR20. Due to the inactivity of the PSR20 working
    group this is a way to already provide interoperability while still
    maintaining forward compatibility. When the current status of PSR20 will
    be released at one point in time the stella-maris/clock package will
    extend the PSR20 interface so that this package becomes immeadiately
    PSR20 compatible without any further work necessary. In the long run the
    stella-maris/clock package will then be marked deprecated so that people
    can then use the PSR20 provided implementation.
    
    Should the implementation of PSR20 change between now and a possible
    release then this interface will still exist and a possible
    implementation will need more code-changes anyhow so this interface will
    still provide some way of interoperability.

--- src/Clock.php
+++ src/Clock.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 namespace Lcobucci\Clock;
 
 use DateTimeImmutable;
+use StellaMaris\Clock\ClockInterface;
 
-interface Clock
+interface Clock extends ClockInterface
 {
     public function now(): DateTimeImmutable;
 }
