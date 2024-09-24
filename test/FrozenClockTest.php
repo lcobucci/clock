4012dd8edb2f472fa3cf4e29c622d00377035f72
Luís Cobucci <lcobucci@gmail.com>
2024-09-24 22:55:33 +0200

    Remove unecessary validation
    By requiring PHP 8.3 we no longer need to have two different
    exceptions to handle.
    
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

--- test/FrozenClockTest.php
+++ test/FrozenClockTest.php
@@ -5,9 +5,7 @@ namespace Lcobucci\Clock;
 
 use DateMalformedStringException;
 use DateTimeImmutable;
-use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\RequiresPhp;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
@@ -57,17 +55,6 @@ final class FrozenClockTest extends TestCase
     }
 
     #[Test]
-    #[RequiresPhp('< 8.3.0')]
-    public function adjustTimeThrowsForInvalidModifierInPhp82(): void
-    {
-        $clock = FrozenClock::fromUTC();
-
-        $this->expectException(InvalidArgumentException::class);
-        $clock->adjustTime('invalid');
-    }
-
-    #[Test]
-    #[RequiresPhp('>= 8.3.0')]
     public function adjustTimeThrowsForInvalidModifier(): void
     {
         $clock = FrozenClock::fromUTC();
